43e6bfeba13c602d2e0bbd7a4af5c692a013230e
nagwan <nagwan.s.abdelhamid@gmail.com>
2019-11-22 19:38:57 +0200

    remove un-used asset

--- src/components/partials/site-nav.jsx
+++ src/components/partials/site-nav.jsx
@@ -5,7 +5,7 @@ const Nav = (() => {
     return (
         <nav className="navbar navbar-expand-sm">
             <Link className="navbar-brand" to="/">
-                <img src={Logo} alt='Football Leagues' />
+                <img src={Logo} alt='Football Leagues' /> <span className='text-white text-bold m-l-10'>FOOTBALL LEAGUES</span>
             </Link>
         </nav>
 
