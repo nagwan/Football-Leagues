24639b34a656379336cae6cb0959099eb73807ba
nagwan <nagwan.s.abdelhamid@gmail.com>
2019-11-22 18:28:56 +0200

    show team

--- src/components/teams/show/container.jsx
+++ src/components/teams/show/container.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import View from './view';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router-dom'
+import { showTeamFlag } from '../../../store/modules/leagues/actions'
 
+
+let _ = require('lodash')
 
 class Container extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.fetchTeam = this.fetchTeam.bind(this);
+    }
+
+    componentDidMount() {
+        
+        this.fetchTeam()
+
+    }
+
+    fetchTeam() {
+        const { match } = this.props;
+
+        this.props.showTeamFlag({ id: match.params.team_id});
+    }
+
+
     render() {
-        return <View />   
+        return (
+            <React.Fragment>
+                {
+                    !_.isEmpty(this.props.Leagues.team) ? <View team={this.props.Leagues.team} /> : null
+                }
+            </React.Fragment>
+        )
     }
 }
 
-export default Container
+export default withRouter(connect(({ Leagues }) => ({ Leagues }),
+    dispatch => bindActionCreators({ showTeamFlag }, dispatch))(Container))
--- src/components/teams/show/view.jsx
+++ src/components/teams/show/view.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import HorizontalCard from '../../partials/team-horizontal-card'
+import Card from '../../partials/player-card'
 
 
-const View = (() => {
+const View = (({ team }) => {
     return (
         <div>
-            <h1>Team show</h1>
+            <div className='row m-b-30 m-t-30'>
+                <div className='col-12'>
+                    <HorizontalCard team={team} />
+                </div>
+            </div>
+
+            <div className='separator'></div>
+
+            <div className='row flex-wrap'>
+                {
+                    team.squad.map(player =>
+                        <div className='col-12 col-md-6 col-lg-4 m-t-10 m-b-10' key={player.id}>
+                            <Card player={player} />
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 })
